42e10721fdaee6b0558e9b67eaa50fdfa08b63f4
Plamen <poky_1983@abv.bg>
2018-04-23 21:35:01 +0300

    23-April-2018

---
+++ 2018-Jan/02. Parking System/02. Parking System.cs
@@ -0,0 +1,107 @@
+﻿namespace _02.Parking_System
+{
+    using System;
+    using System.Linq;
+
+    public class Problem02
+    {
+        private static bool[,] matrix;
+
+        public static void Main()
+        {
+            CreateMatrix();
+
+            ReadParkingSlots();
+        }
+
+        private static void ReadParkingSlots()
+        {
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+                if (inputLine == "stop")
+                {
+                    break;
+                }
+
+                var tokens = inputLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .ToArray();
+                var startRow = tokens[0];
+                var desiredRow = tokens[1];
+                var desiredCol = tokens[2];
+
+                var distance = CalculateDistnece(startRow, desiredRow, desiredCol);
+
+                var result = distance > 0 ? $"{distance}" : $"Row {desiredRow} full";
+
+                Console.WriteLine(result);
+
+            }
+        }
+
+        private static int CalculateDistnece(int startRow, int desiredRow, int desiredCol)
+        {
+            var distance = 0;
+
+            if (IsFreeSlot(desiredRow, desiredCol))
+            {
+                distance = Calculate(desiredRow, startRow, desiredCol);
+                matrix[desiredRow, desiredCol] = true;
+                return distance;
+            }
+
+            var left = desiredCol;
+            var right = desiredCol;
+            var lenght = matrix.GetLength(1);
+
+            while (left > 0 || right < lenght)
+            {
+                left--;
+                if (IsInsideTheParkingSlot(left) && IsFreeSlot(desiredRow, left))
+                {
+                    distance = Calculate(desiredRow, startRow, left);
+                    matrix[desiredRow, left] = true;
+                    break;
+                }
+
+                right++;
+                if (IsInsideTheParkingSlot(right) && IsFreeSlot(desiredRow, right))
+                {
+                    distance = Calculate(desiredRow, startRow, right);
+                    matrix[desiredRow, right] = true;
+                    break;
+                }
+            }
+
+            return distance;
+        }
+
+        private static int Calculate(int desiredRow, int startRow, int desiredCol)
+        {
+            return Math.Abs(desiredRow - startRow) + desiredCol + 1;
+        }
+
+        private static bool IsFreeSlot(int row, int col)
+        {
+            return !matrix[row, col];
+        }
+
+        private static bool IsInsideTheParkingSlot(int left)
+        {
+            return left > 0 && left < matrix.GetLength(1);
+        }
+
+        private static void CreateMatrix()
+        {
+            var dimentions = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var rows = dimentions[0];
+            var cols = dimentions[1];
+
+            matrix = new bool[rows, cols];
+        }
+    }
+}
--- 2018-Jan/ConsoleApplication7/Program.cs
+++ 2018-Jan/ConsoleApplication7/Program.cs
@@ -12,7 +12,37 @@ namespace ConsoleApplication7
 
             var result = string.Join(string.Empty, input.Reverse());
            
-            Console.WriteLine(result);
+           // Console.WriteLine(result);
+
+
+            var arr = new int[] { 1, 2, 3, 1, 7};
+            var input2 = "";
+            var reminder = input == "odd" ? 1 : 0;
+            var arr1 = arr.Where(x => x % 2 == reminder).ToArray();
+
+            Array.LastIndexOf(arr, 1);
+
+            var arr2 = arr.Skip(1).Take(3);
+            var arr3 = arr.Concat(arr2);
+
+            //Console.WriteLine(string.Join(", ", arr3));
+
+
+            var newArr = (int[])arr.Clone();
+            arr[0] = 100;
+            newArr[0] = 111;
+
+           // Console.WriteLine(arr[0]);
+          //  Console.WriteLine(newArr[0]);
+
+            var matrix = new int [5, 5];
+            var matrix2 = (int[,])matrix.Clone();
+            matrix[0, 0] = 100;
+            matrix2[0, 0] = 111;
+
+            Console.WriteLine(matrix[0,0]);
+            Console.WriteLine(matrix2[0,0]);
         }
+        
     }
 }
